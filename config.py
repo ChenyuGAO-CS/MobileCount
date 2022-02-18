b418dc830ec5566b68d29c8b9b69f3a563777583
fabricejourdan <jourdan.fabrice@yahoo.fr>
2022-02-18 15:40:38 +0000

    correctifs

--- config.py
+++ config.py
@@ -36,7 +36,6 @@ __C.LR_DECAY = 0.995 # decay rate
 __C.LR_DECAY_START = -1 # when training epoch is more than it, the learning rate will be begin to decay
 __C.NUM_EPOCH_LR_DECAY = 1 # decay frequency
 __C.MAX_EPOCH = 500
-#__C.MAX_EPOCH = 5
 
 # print 
 __C.PRINT_FREQ = 10
