476fe57ea7da2a7a17b083b88d77e542b573e30d
fabricejourdan <jourdan.fabrice@yahoo.fr>
2022-03-11 16:09:41 +0000

    ok

--- misc/calculate_mean_std.py
+++ misc/calculate_mean_std.py
@@ -23,28 +23,32 @@ tests_dictionary = {
     "SHHA": {"LIST_C_DATASETS": [(CustomSHH, '/workspace/data/shanghaiTech/part_A_final/')],
              "VAL_BATCH_SIZE": 1,                      
              "MEAN_STD_REFERENCE": ([0.410824894905, 0.370634973049, 0.359682112932], [0.278580576181, 0.26925137639, 0.27156367898]),
-             "RECALCULATE": False
+             "RECALCULATE": True
              },
     "SHHB": {"LIST_C_DATASETS": [(CustomSHH, '/workspace/data/shanghaiTech/part_B_final/')],
              "VAL_BATCH_SIZE": 1,  
              "MEAN_STD_REFERENCE": ([0.452016860247, 0.447249650955, 0.431981861591], [0.23242045939, 0.224925786257, 0.221840232611]),
-             "RECALCULATE": False
+             "RECALCULATE": True
              },
     "GCC": {"LIST_C_DATASETS": [(CustomGCC, '/workspace/data/GCC')],
              "VAL_BATCH_SIZE": 1,  
              "MEAN_STD_REFERENCE": ([0.302234709263, 0.291243076324, 0.269087553024], [0.227743327618, 0.211051672697, 0.184846073389]),
-             "RECALCULATE": False
+             "RECALCULATE": True
              },
     "BACKGROUND": {"LIST_C_DATASETS": [(CustomCCLabeler, '/workspace/cclabeler/')],
              "PATH_SETTINGS":{'BG__index_filepath':'/workspace/cclabeler/users/user4.json'},
              "VAL_BATCH_SIZE": 1,  
-             "MEAN_STD_REFERENCE": ([0.45974895, 0.46210653, 0.4612845], [0.26007423, 0.26102763, 0.28212583]),
+             "MEAN_STD_REFERENCE": ([0.45974886, 0.46210667, 0.46128407], [0.2600742, 0.2610275, 0.28212664]),
              "RECALCULATE": True
              },
-    "SHHA+SHHB+GCC": {"LIST_C_DATASETS": [(CustomSHH, '/workspace/data/shanghaiTech/part_A_final/'), (CustomSHH, '/workspace/data/shanghaiTech/part_B_final/')],
+    "SHHA+SHHB+GCC+BACKGROUND": {"LIST_C_DATASETS": [(CustomSHH, '/workspace/data/shanghaiTech/part_A_final/'), 
+                                                     (CustomSHH, '/workspace/data/shanghaiTech/part_B_final/'),
+                                                     (CustomGCC, '/workspace/data/GCC'),
+                                                     (CustomCCLabeler, '/workspace/cclabeler/')],
+             "PATH_SETTINGS":{'BG__index_filepath':'/workspace/cclabeler/users/user4.json'},
              "VAL_BATCH_SIZE": 1,  
-             "MEAN_STD_REFERENCE": ([0.4341471, 0.41423583, 0.40074146], [0.25675747, 0.2502644, 0.24924684]),
-             "RECALCULATE": False
+             "MEAN_STD_REFERENCE": ([0.32202065, 0.311729, 0.29429936], [0.23350027, 0.21823275, 0.19834095]),
+             "RECALCULATE": True
              },
 }
     
