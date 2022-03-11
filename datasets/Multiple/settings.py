10d06e8a18862b74b52de26a8982fe04f3f72443
victorjourne <journe.victor@gmail.com>
2022-03-11 09:56:31 +0100

    Typo

--- datasets/Multiple/settings.py
+++ datasets/Multiple/settings.py
@@ -29,7 +29,7 @@ __C_DYN.RESUME_MODEL = '/data/models'
 __C_DYN.TRAIN_BATCH_SIZE = 3
 __C_DYN.VAL_BATCH_SIZE = 1
 __C_DYN.PATH_SETTINGS = {'GCC__gt_folder': '/workspace/home/gameiroth/data/GCC/density/maps_adaptive_kernel/',
-                         'SHH__gt_name_folder': 'maps_fixed_kernel'}
+                         'SHHB__gt_name_folder': 'maps_fixed_kernel'}
 
 
 #- GCC : 
