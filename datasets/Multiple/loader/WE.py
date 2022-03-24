6b3bdf2ea91c9684d4f7c0125efceae6de22e00e
fabricejourdan <jourdan.fabrice@yahoo.fr>
2022-03-24 13:13:08 +0000

    We classic

---
+++ datasets/Multiple/loader/WE.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import glob
+import os
+import numpy as np
+import logging as lg
+import pathlib
+from scipy.sparse import load_npz
+from .dynamics import CustomDataset
+
+
+class CustomWE(CustomDataset):
+    def __init__(self, folder, mode, **kwargs):
+"""
+        if img.mode == 'L':
+        img = img.convert('RGB')
+        
+    train_main_transform = own_transforms.Compose([
+        own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
+        own_transforms.RandomHorizontallyFlip()
+    ])
+    val_main_transform = None
+    img_transform = standard_transforms.Compose([
+        standard_transforms.ToTensor(),
+        standard_transforms.Normalize(*mean_std)
+    ])
+    gt_transform = standard_transforms.Compose([
+        own_transforms.LabelNormalize(log_para)
+    ])
+    restore_transform = standard_transforms.Compose([
+        own_transforms.DeNormalize(*mean_std),
+        standard_transforms.ToPILImage()
+    ])
+"""
+        super().__init__()
+        self.val_folder = ['104207','200608','200702','202201','500717']
+        self.gt_format = kwargs.get('WE__gt_format', '.csv')
+        self.transform = kwargs.get('WE__transform', None)
+        self.folder = folder
+        self.mode = mode
+        self.dataset = self.read_index()
+        
+    def read_index(self):
+        """
+        Read all images position in WE Dataset
+        """
+        img_list = []
+        if self.mode=='test':
+            for folder in self.val_folder:
+                img_list += list(glob.glob(os.path.join(self.folder, f'{self.mode}', folder, 'img', '*')))
+        else:
+            img_list += list(glob.glob(os.path.join(self.folder, f'{self.mode}', 'img', '*')))
+
+        json_data = {}
+        for n, im in enumerate(img_list):
+            root_dir = Ppathlib.Path(im).parent.parent
+            filename = pathlib.Path(im).stem
+            path_gt = os.path.join(root_dir, 'den', filename + self.gt_format)
+            gt_count = None
+            json_data[n] = {
+                            "path_img": im,
+                            "path_gt": path_gt,
+                            "gt_count": gt_count,
+                            "folder": self.folder
+                            }
+        df = pd.DataFrame.from_dict(json_data, orient='index')
+        return df
+    
+    def load_gt(self, filename):
+        
+        data = pd.read_csv(filename, sep=',',header=None).values
+        data = data.astype(np.float32, copy=False)    
+        density_map = Image.fromarray(data)  
+        
+        self.check_density_map(density_map)
+        return density_map
+    
--- datasets/Multiple/settings.py
+++ datasets/Multiple/settings.py
@@ -1,5 +1,5 @@
 from easydict import EasyDict as edict
-from datasets.Multiple.loader import CustomGCC, CustomSHH, CustomCCLabeler
+from datasets.Multiple.loader import CustomGCC, CustomSHH, CustomCCLabeler, CustomWE
 
 
 # init
@@ -8,12 +8,15 @@ __C_DYN = edict()
 cfg_data = __C_DYN
 
 __C_DYN.IMAGE_SIZE = None
-__C_DYN.TRAIN_SIZE = (576,768)
+#__C_DYN.TRAIN_SIZE = (576,768)
+__C_DYN.TRAIN_SIZE = (512,672) # For WE
 __C_DYN.LIST_C_DATASETS = [#(CustomGCC, '/workspace/data/GCC'), 
                           #(CustomSHH, '/workspace/data/shanghaiTech/part_A_final/'), 
                           (CustomSHH, '/workspace/data/shanghaiTech/part_B_final/'),
+                          (CustomWE, '/workspace/data/worldExpo10_blurred'),
                           #(CustomCCLabeler,  '/workspace/cclabeler/'),
 ]
+
 #__C_DYN.MEAN_STD = ([0.4355689, 0.41689757, 0.41106898], [0.27048737, 0.26903987, 0.28157565]) # SHHA+BACKGROUND
 #__C_DYN.MEAN_STD = ([0.4551607, 0.45352426, 0.44445348], [0.24763596, 0.24388753, 0.25247648]) # SHHB+BACKGROUND
 #__C_DYN.MEAN_STD = ([0.44195193, 0.42883006, 0.41919887], [0.25804275, 0.25455004, 0.26120335]) # SHHA+SHHB+BACKGROUND
@@ -23,10 +26,10 @@ __C_DYN.LIST_C_DATASETS = [#(CustomGCC, '/workspace/data/GCC'),
 #__C_DYN.MEAN_STD = ([0.4355689, 0.41689757, 0.41106898], [0.27048737, 0.26903987, 0.28157565]) # SHHA+BACKGROUN
 ##Rappel
 #__C_DYN.MEAN_STD = ([0.410824894905, 0.370634973049, 0.359682112932], [0.278580576181, 0.26925137639, 0.27156367898]) # SHHA
-__C_DYN.MEAN_STD = ([0.452016860247, 0.447249650955, 0.431981861591], [0.23242045939, 0.224925786257, 0.221840232611]) # SHHB
+#__C_DYN.MEAN_STD = ([0.452016860247, 0.447249650955, 0.431981861591], [0.23242045939, 0.224925786257, 0.221840232611]) # SHHB
 #__C_DYN.MEAN_STD = ([0.302234709263, 0.291243076324, 0.269087553024], [0.227743327618, 0.211051672697, 0.184846073389]) # GCC
 #__C_DYN.MEAN_STD = ([0.45974895, 0.46210647, 0.46128437], [0.26007405, 0.26102796, 0.2821262]) # BACKGROUND
-        
+__C_DYN.MEAN_STD = ([0.504379212856, 0.510956227779, 0.505369007587], [0.22513884306, 0.225588873029, 0.22579960525]) # WE       
         
 #__C_DYN.PROB = [0.2, 0.4, 0.4] # proba getting images
 __C_DYN.COLLATE_FN = True
